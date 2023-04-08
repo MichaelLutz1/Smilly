5cf2b5c0a268794f0f1dc26d7182796feaacff13
Michael Lutz <mikelutz2400@gmail.com>
2023-04-08 10:25:14 -0400

    Initial Commit

---
+++ Smilly/SmillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmillyApp.swift
+//  Smilly
+//
+//  Created by Michael Lutz on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SmillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
